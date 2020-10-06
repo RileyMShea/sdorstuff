2ec414068dae3ea31443d38e0428f22fcc1f46a4
Riley Shea <rileymshea@gmail.com>
2020-10-06 09:02:21 -0400

    updated css

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 import "./styles.css";
+import logo from './sydor_logo.png'
 
 function App() {
   return (
     <div id="App" className="App">
       <div id="logo">
         <a href="https://sydortechnologies.com/">
-          <img src="/sydor_logo.png" alt="Sydor Technologies" />
+          <img src={logo} alt="Sydor Technologies" />
         </a>
       </div>
       <div>
-      <SwaggerUI url="./sydor.yml" />
+      <SwaggerUI url="sydor.yml" />
       </div>
     </div>
   );
